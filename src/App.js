86d5671eebee059312930572ec2a58ea7672aea2
morislaflame <andrey.uraltender@yandex.ru>
2024-07-20 21:20:21 +0500

    Обновление документации

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import { useEffect } from 'react';
 import './App.css';
+import { useTelegram } from "./hooks/useTelegram";
 
-const tg = window.Telegram.WebApp;
 
 function App() {
+  const {onToggleButton, tg} = useTelegram();
 
   useEffect(() => {
     tg.ready();
   }, [])
-
-  const onClose = () => {
-    tg.close()
-  }
-
+  
   return (
     <div className="App">
-      working
-      <button onClick={onClose}>Закрыть</button>
+      <button onClick={onToggleButton}>toggle</button>
     </div>
   );
 }
