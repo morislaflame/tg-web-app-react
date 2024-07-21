f44bc6d0a911ec85a12c42b6856f54fd630a561d
morislaflame <andrey.uraltender@yandex.ru>
2024-07-21 22:04:39 +0500

    Обновление документации

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -36,25 +36,7 @@ const Form = () => {
 
     return (
         <div className={'form'}>
-            <h3>Введите ваши данные</h3>
-            <input 
-                className={'input'} 
-                type="text" 
-                placeholder={'Страна'} 
-                value={country} 
-                onChange={onChangeCountry}
-            />
-            <input 
-                className={'input'} 
-                type="text" 
-                placeholder={'Улица'}
-                value={street} 
-                onChange={onChangeStreet}
-            />
-            <select className={'select'} value={subject} onChange={onChangeSubject}>
-                <option value={'physical'}>Физ. лицо</option>
-                <option value={'legal'}>Юр. лицо</option>
-            </select>
+            Form
         </div>
     );
 };
